c26f4d15536faac54e372b05a8ec19924badfe2d
coolgray06 <6636@holbertonstudents.com>
2023-08-24 09:02:40 -0700

    leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Print the size of tree.
+ * @tree: Tree to print leaves
+ * Return: Depth
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	int leaves = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return (leaves);
+}
